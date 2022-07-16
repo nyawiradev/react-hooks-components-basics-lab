e0868ecd86b0235ed53d2b1fcca67113ddb17c55
nyawiradev <njaginjoyce@gmail.com>
2022-07-16 17:04:09 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -17,14 +17,28 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About() {
+  return (
+    <div id="About">
+      <h2>About</h2>
+    </div>
+  )
+
+
+}
+   
+  
+  
+  
+
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      {<Home/>}
+      {<About/>}
     </div>
   );
 }
